a131082e480bc301c126675f4edb3a6635109679
TariqT777 <TariqThomas777@gmail.com>
2021-09-22 16:39:37 -0400

    Added a stylesheet and JS page.

---
+++ action.js
@@ -0,0 +1,13 @@
+document.getElementById('Account-Button').onclick = function() {
+  alert("button was clicked");
+}​;​
+
+/*
+document.addEventListener("click", function (event) {
+    // Checking if the button was clicked
+    if (!event.target.matches("#Account-Button")) return;
+  
+    console.log("Button was clicked!");
+  });
+
+*/
