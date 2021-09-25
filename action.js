fb91f8ed2467f1f1fe809acd07580776eddf4a52
TariqT777 <TariqThomas777@gmail.com>
2021-09-24 20:11:32 -0400

    Beginning to implement random password API

--- action.js
+++ action.js
@@ -1,3 +1,8 @@
+fetch("https://www.passwordrandom.com/api")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+  
 function randomAccount(){
   alert('button was clicked!!!');
 }
