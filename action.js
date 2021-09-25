23481e36218cc10210459ece225ae72b20748585
TariqT777 <TariqThomas777@gmail.com>
2021-09-25 19:10:31 -0400

    HTML is receiving data from API, but is returning Undefined to screen.

--- action.js
+++ action.js
@@ -4,8 +4,14 @@
 
   
 function randomAccount(){
-  fetch("https://www.passwordrandom.com/query?command=password&format=json&count=10#")
+  fetch("https://www.passwordrandom.com/query?command=password&format=json&count=1#")
     .then((response) => response.json())
-    .then((data) => console.log(data));
-    alert('button was clicked!!!');
+    .then((data) => renderPassword(data));
+    //alert('button was clicked!!!');
+}
+
+function renderPassword(data) {
+  const Password = document.getElementById("Password");
+  Password.innerHTML = data.Password;
+  
 }
